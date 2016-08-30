36d932b2378f58318a608f3b8693f0a450c03239
gavinshao111 <yiyecheer@163.com>
2016-08-30 13:29:25 +0800

    MqForEasyD

---
+++ MqForEasyD/strlfunc.h
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   strlcat.h
+ * Author: 10256
+ *
+ * Created on 2016年8月28日, 上午11:12
+ */
+
+#ifndef STRLCAT_H
+#define STRLCAT_H
+
+size_t strlcat(char *dst, const char *src, size_t siz);
+size_t strlcpy(char *dst, const char *src, size_t siz);
+
+#endif /* STRLCAT_H */
+
