c715cd50e13141c9bae19c8a752d241e04f97ab9
gavinshao111 <yiyecheer@163.com>
2016-09-11 09:59:23 +0800

    update header file.

--- MqForEasyD/mainProcess.h
+++ MqForEasyD/mainProcess.h
@@ -1,16 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   mainProcess.h
- * Author: 10256
- *
- * Created on 2016骞�8鏈�28鏃�, 涓婂崍11:06
- */
-
 #ifndef MAINPROCESS_H
 #define MAINPROCESS_H
 
