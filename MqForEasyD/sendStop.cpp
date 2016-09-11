d36612bcc996eefdcdcbecb27c76bc7b3b740d75
gavinshao111 <yiyecheer@163.com>
2016-09-11 15:57:54 +0800

    add sendStop

---
+++ MqForEasyD/sendStop.cpp
@@ -0,0 +1,25 @@
+//g++ sendStop.cpp -L dist/Release/GNU-Linux/ -lMqForEasyD -o sendStop
+#include <stdio.h>
+#include <iostream>
+#include "mainProcess.h"
+using namespace std;
+
+int main(void)
+{
+	
+	const char *fStreamName = "record/$carleapmotorCLOUDE20160727inform/1/2016-08-30_113613.sdp";
+	int rc = -1;
+	
+	
+	while(getchar()){
+		rc = sendStopPushMqWhenThereIsNoClient(fStreamName);
+		if (0 != rc){
+			cout<<"error, rc = "<<rc<<endl;
+			return 0;
+		}
+		cout<<"sent."<<endl;
+	}
+
+
+	return 0;
+}
