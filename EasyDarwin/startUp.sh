163b0d8fe301de203afbae6ea6c90b96a778ce1c
gavinshao111 <yiyecheer@163.com>
2016-09-23 16:04:20 +0800

    update startup.sh

--- EasyDarwin/startUp.sh
+++ EasyDarwin/startUp.sh
@@ -1,4 +1,5 @@
 #echo -e "\n\n" > Logs/running.log
 date > Logs/running.log
-./x64/easydarwin -c WinNTSupport/easydarwin.xml -d >> Logs/running.log &
+./x64/easydarwin -c WinNTSupport/easydarwin.xml -d >> Logs/running.log 2>Logs/stderr.log &
+
 
